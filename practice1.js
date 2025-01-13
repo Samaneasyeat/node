2a3b5df49fd84e5215bb57ec96b68add4d48e815
saman <saman@easyeat.ai>
2025-01-13 16:28:04 +0530

    fixes

--- practice1.js
+++ practice1.js
@@ -1,1 +1,10 @@
-//how to find duplicate element in given array ;
+//what is hoisting give a example 
+console.log("the value of x before initialization of x", x);
+getName();
+var x = 5;
+console.log("the value of x after initialization of x", x);
+function getName() {
+    console.log("get name")
+}
+getName();
+
